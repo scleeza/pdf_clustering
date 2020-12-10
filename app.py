6e18f916bf1bdca62b756135efa8cd26ef12fcd1
scleeza <jacaba0815@gmail.com>
2020-12-10 17:23:05 -0600

    Main

--- app.py
+++ app.py
@@ -1,13 +1,89 @@
 import streamlit as st
-from wikiscraper import wiki_scraper
-import base64
+import spacy_streamlit
+import spacy
+from transformers import pipeline, AutoTokenizer
 
-url = st.text_input('please input url!')
-if len(url) > 0:
-    df = wiki_scraper(url)
-    st.dataframe(df)
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}" download="wiki_table.csv">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-    st.markdown(href, unsafe_allow_html=True)
 
+def main():
+    # Once we have the dependencies, add a selector for the app mode on the sidebar.
+    st.sidebar.title("What to do")
+    app_mode = st.sidebar.selectbox("Choose the app mode",
+        ["Show instructions", "NLP", "Experiment"])
+    if app_mode == "Show instructions":
+        #st.sidebar.success('To continue select "Run the app".')
+        st.write('Paste text into context column and type question you want to know')
+    elif app_mode == "Experiment":
+        run_the_exp()
+    elif app_mode == "NLP":
+        run_the_app()
+
+
+@st.cache(allow_output_mutation=True)
+def load_Roberta_model():
+    model_name = "deepset/roberta-base-squad2"
+    qa_model = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    return qa_model
+
+def get_answer(context,question,model):
+    QA_input = {
+        'question': question,
+        'context': context
+    }
+    res = model(QA_input)
+    return res['answer']
+
+
+def run_the_exp():
+    placeholder = st.empty()
+    options = st.sidebar.radio('Choose expression',('raw_text', 'NER'))
+    with placeholder.beta_container():
+        if options =='raw_text':
+            st.write("This is one element")
+        elif options =='NER':
+            st.write("This is another")
+
+
+@st.cache(allow_output_mutation=True)
+def get_questions():
+    return []
+
+
+def run_the_app():
+    st.title('Feature Extraction based on Roberta model')
+    with st.spinner('Wait for it...'):
+        qa_model = load_Roberta_model()
+    # list of questions
+    context_region = st.empty()
+
+    context = context_region.text_area(label='Context')
+    question = st.text_area(label='Question')
+    if st.button('Query'):
+        if question not in get_questions():
+            get_questions().append(question)
+        answer = get_answer(context=context, question=question, model=qa_model)
+        with st.beta_expander(label=question):
+            st.write(answer)
+    # for i in get_questions():
+    #     st.write(i)
+
+    # left_column, right_column = st.beta_columns(2)
+    # context = left_column.text_area(label='''
+    # Context of Qustions
+    # ''')
+    # question = right_column.text_area(label='''
+    # Qustions
+    # ''')
+    # if left_column.button('Query'):
+    #     answer = get_answer(context=context, question=question, model=qa_model)
+    #     st.write(answer)
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(context)
+    spacy_streamlit.visualize_ner(doc, labels=nlp.get_pipe("ner").labels,title="Context_NER",
+        sidebar_title=None,show_table=False)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
